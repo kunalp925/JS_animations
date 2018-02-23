ce32dc023932ea26609fd46219920fcea5601325
Kunal Patel <kunalpatel@Kunals-MacBook-Pro.local>
2018-02-23 10:46:51 -0800

    adding bubbles growing

---
+++ bubbles growing/index.js
@@ -0,0 +1,159 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var color = [ "#336699" , "#666666", "#999999"];
+var c = canvas.getContext('2d');
+
+console.log("Width: " + innerWidth);
+console.log("Height: " + innerHeight);
+
+var maxRadius = 40;
+
+var circle = [],
+    SIZE = 500;
+
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener("mousemove", function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
+
+
+for(var i = 0; i<SIZE; i++){
+  var radius = Math.floor((Math.random()*8)+2),
+      x = Math.floor(Math.random()*innerWidth)+radius,
+      y = Math.floor(Math.random()*innerHeight)+radius,
+      dx = Math.floor(((Math.random()-0.5)*2)),
+      dy = Math.floor(((Math.random()-0.5)*2)),
+      colors = color[Math.floor((Math.random()*4))];
+      fill = color[Math.floor((Math.random()*4))],
+      minRadius = radius;
+
+      if(x > innerWidth-radius || x < radius){
+        x = Math.floor(Math.random()*innerWidth)+radius;
+      }
+      if(y > innerHeight - radius || y < radius){
+        y = Math.floor(Math.random()*innerHeight)+ radius;
+      }
+      if(dx == 0){
+        dx = Math.floor(((Math.random()-0.5)*3));
+      }
+      if(dy==0){
+        dy = Math.floor(((Math.random()-0.5)*3));
+      }
+
+      console.log("Color: " + colors);
+      console.log("Starting x loc: " + x);
+      console.log("Starting y loc: " + y);
+      console.log("Starting dx: " + dx);
+      console.log("Starting dy: " + dy);
+      console.log("Radius: " + radius);
+
+      circle.push(new Circle(x, y, dx, dy, radius, colors, fill));
+}
+
+
+function Circle(x, y, dx, dy, radius, color, fill){
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.color = color;
+  this.fill = fill;
+  this.minRadius = radius;
+
+  this.draw = function(){
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+    c.strokeStyle = this.color;
+    // c.stroke();
+    c.fillStyle = this.fill;
+    c.fill();
+  }
+
+  this.update = function(){
+    if(this.x > innerWidth - this.radius || this.x < this.radius){
+      this.dx =-this.dx;
+    }
+    if(this.y > innerHeight - this.radius || this.y < this.radius){
+      this.dy =- this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if ((Math.abs(mouse.x - this.x) < 50) && (Math.abs(mouse.y - this.y) < 50)){
+      if(this.radius < maxRadius) {
+        this.radius += 1;
+      }
+
+    }
+
+    else if(this.radius > this.minRadius) {
+        this.radius -= 1;
+    }
+
+
+    this.draw();
+  }
+}
+
+function init(){
+  circle = []
+
+  for(var i = 0; i<SIZE; i++){
+    var radius = Math.floor((Math.random()*10)),
+        x = Math.floor(Math.random()*innerWidth)+radius,
+        y = Math.floor(Math.random()*innerHeight)+radius,
+        dx = Math.floor(((Math.random()-0.5)*2)),
+        dy = Math.floor(((Math.random()-0.5)*2)),
+        colors = color[Math.floor((Math.random()*4))];
+        fill = color[Math.floor((Math.random()*4))],
+        minRadius = radius;
+
+        if(x > innerWidth-radius || x < radius){
+          x = Math.floor(Math.random()*innerWidth)+radius;
+        }
+        if(y > innerHeight - radius || y < radius){
+          y = Math.floor(Math.random()*innerHeight)+ radius;
+        }
+        if(dx == 0){
+          dx = Math.floor(((Math.random()-0.5)*3));
+        }
+        if(dy==0){
+          dy = Math.floor(((Math.random()-0.5)*3));
+        }
+
+        console.log("Color: " + colors);
+        console.log("Starting x loc: " + x);
+        console.log("Starting y loc: " + y);
+        console.log("Starting dx: " + dx);
+        console.log("Starting dy: " + dy);
+        console.log("Radius: " + radius);
+
+        circle.push(new Circle(x, y, dx, dy, radius, colors, fill));
+  }
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for(var r = 0; r < SIZE; r++){
+    circle[r].update();
+  }
+
+}
+
+animate();
